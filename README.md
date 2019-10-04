5c03b04f4691ca7afdf4c72220ca4ac7a2f61e1a
William Teder <wteder@hydreon.com>
2019-10-04 15:45:43 -0500

    feat(app): read jobs from files, work with user agent strings

--- README.md
+++ README.md
@@ -3,5 +3,14 @@
 Provides functionality for the api, investigates a potentially malicious website safely, on behalf of a security researcher.
 
 ## Features
+- Grabs screenshots of the target URL, with customizable browser viewport (width/height)
+- Accepts a User-Agent string to pass to the website
+
+### Upcoming
 - Beautified JS: Web service will include a JS package to beautify JS string	
-- Screenshots: Web service will have the ability to return screenshots to users
+
+## Usage
+- Run `npm install` to set up puppeteer.
+- Use `npm start` to run the program.
+- Place files into the `input` directory, based on the syntax of `request.json`.
+- Look in the `output/` directory, the filename of the input will match to the output
--- app.mjs
+++ app.mjs
@@ -1,9 +1,73 @@
 import puppeteer from 'puppeteer';
+import {promises as fs} from 'fs';
 
-puppeteer.launch().then(async browser => {
-    const page = await browser.newPage();
-    await page.goto('https://www.google.com');
-    await page.screenshot({path: 'example.png'});
-    // other actions...
+(async () => {
+    console.log("Launching browser...");
+    const browser = await puppeteer.launch();
+
+    console.log("Reading list of jobs...");
+    let input, output;
+
+    // read in list of files from input and output dirs:
+    try {
+        input = await fs.readdir('input');
+        output = await fs.readdir('output');
+    } catch(e) {
+        console.warn("Failed to read input/output directories.");
+
+        process.exit(0);
+    }
+
+    // find .json files in input dir:
+    let tickets = input.filter(filename => filename.endsWith(".json"));
+
+    // filter output dir to only be folders, not files:
+    const finishedTickets = output.filter(filename => !filename.includes("."));
+
+    // check which .json files don't have folders in output/:
+    const unfinished = tickets.filter(filename => !finishedTickets.includes(filename.replace(".json", "")));
+
+    for(let ticket of unfinished) {
+        const ticketNumber = ticket.replace(".json", "");
+
+        console.log(`Start processing for ${ticketNumber}.`);
+
+        try {
+            let contents = await fs.readFile(`input/${ticket}`, "utf8");
+
+            contents = JSON.parse(contents);
+
+            // make output/{ticketNumber}:
+            fs.mkdir(`output/${ticketNumber}`);
+
+            // create a new page in puppeteer:
+            const page = await browser.newPage();
+            await page.setViewport({
+                width: contents.screenshot.width,
+                height: contents.screenshot.height,
+                deviceScaleFactor: 1,
+            });
+
+            console.log(`Loading ${contents.url}...`);
+            await page.goto(contents.url);
+
+            const userAgent = contents.useragent || await browser.userAgent();
+            console.log(`Setting user agent string to ${userAgent}.`);
+            page.setUserAgent(userAgent);
+
+            const screenshotFile = `output/${ticketNumber}/${contents.screenshot.filename}`;
+            console.log(`Saving screenshot to ${screenshotFile}`);
+            await page.screenshot({path: screenshotFile});
+
+            console.log("Done, closing page.");
+            await page.close();
+        } catch(e) {
+            console.warn(`Failed to load ticket: ${ticket}`);
+
+            console.log(e);
+        }
+    }
+
+    console.log(`Finished ${tickets.length} jobs! Closing browser.`);
     await browser.close();
-});
+})();
