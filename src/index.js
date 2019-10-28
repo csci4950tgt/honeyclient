9bc236d5615319132494b560a51cd75005bc6a64
Rafael Barash <rafabarash@gmail.com>
2019-10-28 14:13:35 -0500

    added basic prettier config to project

--- src/index.js
+++ src/index.js
@@ -5,100 +5,116 @@ import db from './manager/DatabaseManager';
 const FULLPAGE_VIEWPORT_WIDTH = 1920;
 const FULLPAGE_VIEWPORT_HEIGHT = 1080;
 
-(async () => {
-    console.log("Launching browser...");
-    const browser = await puppeteer.launch();
+const processTicket = async () => {
+  const ticketId = ticket.get('id');
+  const ticketURL = ticket.get('url');
 
-    console.log("Finding new tickets...");
-    const tickets = await db.getNewTickets();
+  // pull the fields out of the screenshot object into something a bit easier to work with:
+  const ticketScreenshots = ticket.get('ScreenShots').map(ss => ({
+    ticketId: ss.get('TicketId'),
+    width: ss.get('width'),
+    height: ss.get('height'),
+    filename: ss.get('filename'),
+    userAgent: ss.get('userAgent'),
+  }));
 
-    for(let ticket of tickets) {
-        const ticketId = ticket.get("id");
-        const ticketURL = ticket.get("url");
+  // any objects in here will get stored:
+  const artifacts = [];
 
-        // pull the fields out of the screenshot object into something a bit easier to work with:
-        const ticketScreenshots = ticket.get("ScreenShots").map(ss => ({
-            ticketId: ss.get("TicketId"),
-            width: ss.get("width"),
-            height: ss.get("height"),
-            filename: ss.get("filename"),
-            userAgent: ss.get("userAgent")
-        }));
+  console.log(`Start processing for ticket #${ticketId}.`);
 
-        // any objects in here will get stored:
-        const artifacts = [];
+  try {
+    // create a new page in puppeteer:
+    const page = await browser.newPage();
 
-        console.log(`Start processing for ticket #${ticketId}.`);
+    console.log(`Loading ${ticketURL}...`);
+    await page.goto(ticketURL);
 
-        try {
-            // create a new page in puppeteer:
-            const page = await browser.newPage();
+    // process all screenshots:
+    for (let ss of ticketScreenshots) {
+      // +1 is intentional, extra fullpage screenshot should count towards total:
+      console.log(
+        `Processing screenshot ${ticketScreenshots.indexOf(ss) +
+          1} of ${ticketScreenshots.length + 1}.`
+      );
 
-            console.log(`Loading ${ticketURL}...`);
-            await page.goto(ticketURL);
+      await page.setViewport({
+        width: ss.width,
+        height: ss.height,
+        deviceScaleFactor: 1,
+      });
 
-            // process all screenshots:
-            for(let ss of ticketScreenshots) {
-                // +1 is intentional, extra fullpage screenshot should count towards total:
-                console.log(`Processing screenshot ${ticketScreenshots.indexOf(ss) + 1} of ${ticketScreenshots.length + 1}.`);
+      const userAgent = ss.userAgent || (await browser.userAgent());
+      console.log(`Setting user agent string to ${userAgent}.`);
+      page.setUserAgent(userAgent);
 
-                await page.setViewport({
-                    width: ss.width,
-                    height: ss.height,
-                    deviceScaleFactor: 1
-                });
-
-                const userAgent = ss.userAgent || await browser.userAgent();
-                console.log(`Setting user agent string to ${userAgent}.`);
-                page.setUserAgent(userAgent);
-
-                // data object is a buffer, we'll convert it later:
-                artifacts.push({
-                    screenshot: ss,
-                    data: await page.screenshot()
-                });
-            }
-
-            // take full-page screenshot:
-            console.log(`Processing screenshot ${ticketScreenshots.length + 1} of ${ticketScreenshots.length + 1}.`);
-
-            await page.setViewport({
-                width: FULLPAGE_VIEWPORT_WIDTH,
-                height: FULLPAGE_VIEWPORT_HEIGHT,
-                deviceScaleFactor: 1
-            });
-
-            // the last user-agent is re-used to take the full-page screenshot:
-            artifacts.push({
-                screenshot: {
-                    ticketId: ticketId,
-                    width: FULLPAGE_VIEWPORT_WIDTH,
-                    height: FULLPAGE_VIEWPORT_HEIGHT,
-                    filename: "screenshotFull.png",
-                    userAgent: await browser.userAgent()
-                },
-                data: await page.screenshot({ fullPage: true })
-            });
-
-            console.log("Saving output to database...");
-            for(let obj of artifacts) {
-                console.log(`Saving object ${obj.screenshot.filename}...`);
-
-                await db.storeFile(obj.screenshot.ticketId, obj.screenshot.filename, obj.data);
-            }
-
-            console.log("Marking ticket as processed...");
-            await db.closeTicketById(ticketId);
-
-            console.log("Done, closing page.");
-            await page.close();
-        } catch(e) {
-            console.warn("An error occurred when processing a ticket:");
-
-            console.log(e);
-        }
+      // data object is a buffer, we'll convert it later:
+      artifacts.push({
+        screenshot: ss,
+        data: await page.screenshot(),
+      });
     }
 
-    console.log(`Finished ${tickets.length} tickets! Closing browser.`);
-    await browser.close();
-})();
+    // take full-page screenshot:
+    console.log(
+      `Processing screenshot ${ticketScreenshots.length +
+        1} of ${ticketScreenshots.length + 1}.`
+    );
+
+    await page.setViewport({
+      width: FULLPAGE_VIEWPORT_WIDTH,
+      height: FULLPAGE_VIEWPORT_HEIGHT,
+      deviceScaleFactor: 1,
+    });
+
+    // the last user-agent is re-used to take the full-page screenshot:
+    artifacts.push({
+      screenshot: {
+        ticketId: ticketId,
+        width: FULLPAGE_VIEWPORT_WIDTH,
+        height: FULLPAGE_VIEWPORT_HEIGHT,
+        filename: 'screenshotFull.png',
+        userAgent: await browser.userAgent(),
+      },
+      data: await page.screenshot({ fullPage: true }),
+    });
+
+    console.log('Saving output to database...');
+    for (let obj of artifacts) {
+      console.log(`Saving object ${obj.screenshot.filename}...`);
+
+      await db.storeFile(
+        obj.screenshot.ticketId,
+        obj.screenshot.filename,
+        obj.data
+      );
+    }
+
+    console.log('Marking ticket as processed...');
+    await db.closeTicketById(ticketId);
+
+    console.log('Done, closing page.');
+    await page.close();
+  } catch (e) {
+    console.warn('An error occurred when processing a ticket:');
+
+    console.log(e);
+  }
+};
+
+const main = async () => {
+  console.log('Launching browser...');
+  const browser = await puppeteer.launch();
+
+  console.log('Finding new tickets...');
+  const tickets = await db.getNewTickets();
+
+  for (let ticket of tickets) {
+    processTicket(ticket);
+  }
+
+  console.log(`Finished ${tickets.length} tickets! Closing browser.`);
+  await browser.close();
+};
+
+main();
