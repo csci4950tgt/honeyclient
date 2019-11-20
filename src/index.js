e6bac791f822256135c4cb6e4b253546a2b25f7e
William Teder <wteder@hydreon.com>
2019-11-20 15:10:22 -0600

    feat(index): relaunch browser on disconnection, keep browser open

--- src/index.js
+++ src/index.js
@@ -3,30 +3,42 @@ import db from './utils/db';
 import ticketManager from './utils/tickets';
 import PGPubsub from 'pg-pubsub';
 
+let browser;
+
+const processAllTickets = async () => {
+  console.log('Finding new tickets...');
+  const tickets = await db.getNewTickets();
+
+  try {
+    // loop through and process tickets
+    for (let ticket of tickets) {
+      await ticketManager.processTicket(browser, ticket);
+    }
+    // success message
+    console.log(
+      `Finished processing ${tickets.length} ticket(s)! Awaiting more...`
+    );
+  } catch (e) {
+    console.error('An error occurred when processing a ticket');
+    console.log(e);
+  }
+};
+
+const setupBrowser = async () => {
+  browser = await puppeteer.launch();
+
+  browser.on('disconnected', setupBrowser);
+};
+
 const main = async () => {
   console.log('Launching browser...');
-  const browser = await puppeteer.launch();
+  await setupBrowser();
 
-  db.registerUpdateHandler(async () => {
-    console.log('Finding new tickets...');
-    const tickets = await db.getNewTickets();
+  console.log('Processing existing tickets since last run...');
+  await processAllTickets();
 
-    try {
-      // loop through and process tickets
-      for (let ticket of tickets) {
-        await ticketManager.processTicket(browser, ticket);
-      }
-      // success message
-      console.log(
-        `Finished processing ${tickets.length} ticket(s)! Awaiting more...`
-      );
-    } catch (e) {
-      console.error('An error occurred when processing a ticket');
-      console.log(e);
-    }
-  });
-  console.log(`Closing browser...`);
-  await browser.close();
+  console.log('Waiting for new ticket notification.');
+  db.registerUpdateHandler(processAllTickets);
 };
 
 main();
