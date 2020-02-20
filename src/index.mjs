7c221ac2b542b3a9adb255ff315d65f557ba9abc
Rafael Barash <rafabarash@gmail.com>
2020-02-19 18:30:44 -0600

    removed cors stuff because no need for it, and added google safe browsing key into vs for now to ease development, obvi should change later

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
 
 // Routes
 import ticketRouter from './routes/ticket.js';
@@ -14,25 +13,6 @@ const port = process.env.PORT || 8000;
 
 // Set up express server
 const app = express();
-
-// Cors
-// const allowedOrigins = ['http://localhost:3000', 'http://localhost:8080'];
-// app.use(
-//   cors({
-//     origin: function(origin, callback) {
-//       // allow requests with no origin
-//       // (like mobile apps or curl requests)
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         var msg =
-//           'The CORS policy for this site does not ' +
-//           'allow access from the specified Origin.';
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
 
 // Other middleware
 app.use(express.json());
